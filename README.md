3494ae48eb895d54f1381d029495036f2ae7159a
gquadrati <greta.quadrati@pagopa.it>
2021-02-15 15:24:19 +0100

    [#176589537] scripts to facilitate adding and removing files from repos

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # io-platform-scripts
-a useful group of scripts to maximize the management of severalpagopa's repo together
+
+A useful group of scripts to maximize the management of severalpagopa's repo together
+
+## add-file-to-repo
+
+### Prerequisites
+
+- Having _hub_ command installed
+- Setup _hub_ config file with _username_ and _Github Personal Access Token_ like this:
+
+```bash
+    echo "github.com:
+        - user: _<github username>_
+          oauth_token: _<github token>_" > ~/.config/hub
+```
+
+### Example
+
+#### One project
+
+```bash
+add-file-to-repo.sh io-functions-app .devops/yarn-lock-upgrade.yml io/io-functions-test-deploy . test-branch "Test PR Title" "Test PR Description"
+```
+
+#### Multiple projects
+
+```bash
+echo "io-functions-test-deploy\nio-functions-template" | xargs -I{} ./scripts/add-file-to-repo.sh {}  .devops/yarn-lock-upgrade.yml io/io-functions-test-deploy . test-branch "Test PR Title" "Test PR Description" 
+```
+
+## 2) remove-file-from-repo
+
+### Prerequisites
+
+Same as ['add-file-to-repo'](#add-file-to-repo)
+
+### Example
+
+#### One project
+
+```bash
+remove-file-from-repo.sh io-functions-app  yarn-lock-upgrade.yml .devops test-branch "Test PR Title" "Test PR Description"
+```
+
+#### Multiple projects
+
+```bash
+echo "io-functions-test-deploy\nio-functions-template" | xargs -I{} ./scripts/remove-file-from-repo {}  yarn-lock-upgrade.yml .devops test-branch "Test PR Title" "Test PR Description" 
+```
---
+++ add-file-to-repo.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# -------------------------------------------------------
+# A shell script to add  file to a repo and make a PR
+# -------------------------------------------------------
+
+
+usage="usage: add-file-to-repo [github_repo] [file_name] [from_folder] [to_folder] [branch_name] [pr_title] [pr_description]
+    where:
+    - github_repo: github repo where file needs to be added
+    - file_name: the name of the file to add
+    - from_folder: the path of the folder where the file is present
+    - to_folder: the path of the folder where the file has to be added
+    - branch_name: the name of the branch to create
+    - pr_title: the title of the PR
+    - pr_description: the description of the PR
+"
+
+if [[ ! $# -eq 7 ]] ; then
+    echo "$usage"
+    exit 1
+fi
+
+
+inputs=( "$@" )
+get_input()
+{
+    case $1 in
+     "github_repo")
+        echo "${inputs[0]}"
+        ;;
+     "file_name")
+        echo "${inputs[1]}"
+        ;;
+     "from_folder")
+        echo "${inputs[2]}"
+        ;;
+     "to_folder")
+        echo "${inputs[3]}"
+        ;;
+     "branch_name")
+        echo "${inputs[4]}"
+        ;;
+     "pr_title")
+        echo "${inputs[5]}"
+        ;;
+     "pr_description")
+        echo "${inputs[6]}"
+        ;;
+
+     esac
+}
+
+
+repo="$(get_input "github_repo")"
+branch_name="$(get_input "branch_name")"
+file_name="$(get_input "file_name")"
+from_folder="$(get_input "from_folder")"
+to_folder="$(get_input "to_folder")"
+pr_title="$(get_input "pr_title")"
+pr_description="$(get_input "pr_description")"
+
+git clone "git@github.com:pagopa/$repo.git"
+
+cp "$from_folder/$file_name" "$repo/$to_folder/$file_name"
+
+cd "$repo" || exit 1;
+git checkout -b "$branch_name"
+
+git add "$to_folder/$file_name"
+git commit -m "$pr_title"
+
+git push origin "$branch_name"
+hub pull-request -m "$pr_title" -m "$pr_description"
+
+pr_num=$(hub pr list | grep "$pr_title" | awk '{print $1}' | sed 's/#//')
+echo "PR #$pr_num has been created in repo $repo"
+
+#hub pr show "$pr_num"
+
+cd ..
